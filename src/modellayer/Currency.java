0be1a9257f39e5661babb1a381eaa6e45ab17ba1
Krisztián Henrik Papp <h3nk4s@henky.me>
2020-03-03 22:39:00 +0100

    Add tests for when an invalid coin is inserted after a valid

--- src/modellayer/Currency.java
+++ src/modellayer/Currency.java
@@ -2,12 +2,12 @@ package modellayer;
 
 /**
  * Inspired by the book: Flexible, Reliable Software
- * Henrik Bærbak Christensen: Flexible, Reliable Software. Taylor and Francis Group, LLC 2010
+ * Henrik Bï¿½rbak Christensen: Flexible, Reliable Software. Taylor and Francis Group, LLC 2010
  */
 
 public class Currency {
 	
-	public enum ValidCurrency { EURO, DKK, SEK, NOK };
+	public enum ValidCurrency { EURO, DKK, SEK, NOK, USD };
 	public enum ValidCoinType { FRACTION, INTEGER };
 
 }
