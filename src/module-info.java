8570870090c8db461f5835c09c3201808f5b2769
ninjistile <remistile@gmail.com>
2020-03-03 15:50:25 +0100

    Test illegal coin filled (EURO, NOK)

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module tests {
+	exports test;
+	exports controllayer;
+	exports utility;
+	exports databaselayer;
+	exports modellayer;
+
+	requires java.sql;
+	requires junit;
+}
--- src/test/TestIllegalCoin.java
+++ src/test/TestIllegalCoin.java
@@ -3,6 +3,7 @@ package test;
 import org.junit.*;
 
 import controllayer.*;
+import modellayer.Currency;
 
 /**
  * Inspired by the book: Flexible, Reliable Software Henrik B?rbak Christensen:
@@ -23,14 +24,21 @@ public class TestIllegalCoin {
 	 * Verify that illegal coins are rejected.
 	 */
 	
+	
 	// Norwegian coin
 	@Test(expected = IllegalCoinException.class)
 	public void shouldRejectIllegalCurrencyNokCoin() throws IllegalCoinException {
-		//
+		int amount = 34;
+		Currency.ValidCurrency currency = Currency.ValidCurrency.NOK;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		ps.addPayment(amount,currency, coinType);
 	}
 	// unknown Euro coin value
 	@Test(expected = IllegalCoinException.class)
 	public void shouldRejectIllegalEuroCoin() throws IllegalCoinException {
-		//
+		int amount = 4;
+		Currency.ValidCurrency currency = Currency.ValidCurrency.EURO;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		ps.addPayment(amount,currency, coinType);
 	}
 }
